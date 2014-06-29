ec25cc006028e2c2a622207107b2ceb32749abee
Mike Zhang <mzhang6@gmail.com>
2014-06-29 00:04:08 -0400

    Finished webpage pre-ajax

--- public/js/export.js
+++ public/js/export.js
@@ -8,12 +8,46 @@
       console.log("Event focus: " + $('#event').is(':focus'));
     };
 
+    var selected = function(el) {
+
+    }
+
+    var deselected = function() {
+      var mem = $('#membership').is(':focus');
+      var eve = $('#event').is(':focus');
+      if (!mem && !eve) {
+        $('#selected').text("No report type selected.");
+        $('#selected').css("color", "red");
+        $('#download').addClass("notReady");
+      }
+    }
+
     $('#membership').on('click', function() {
-      printThing();
+      // printThing();
+      $('#selected').text("Selected membership report.");
+      $('#selected').css("color", "green");
+      $('#download').removeClass("notReady");
     });
 
     $('#event').on('click', function() {
-      printThing();
+      // printThing();
+      $('#selected').text("Selected event report.");
+      $('#selected').css("color", "green");
+      $('#download').removeClass("notReady");
     });
+
+    $(document).on('click', function() {
+      deselected();
+    })
+
+    /*
+    $('#membership').on('focusout', function() {
+      deselected();
+    });
+
+    $('#event').on('focusout', function() {
+      deselected();
+    })
+    */
   });
 })();
