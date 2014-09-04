a9eea58b02cefbdd9375440bbdd8ba95201dd2ec
Mike Zhang <mzhang6@gmail.com>
2014-09-04 16:10:29 -0400

    Completed monthly programming report

--- util/DataApiCall.js
+++ util/DataApiCall.js
@@ -72,7 +72,7 @@ var getClubNameInContext = function(dbClubName) {
  * @param relevantDate {date} - The date whose month will be used to fetch api
  * @param done {function} - The callback function of the form (err, data)
  */
-function getMonthlyProgramming(relevantDate, done) {
+function getMonthlyProgramming(relevantDate, region, done) {
   /* Helper functions */
   var genCountsObject = function(title) {
     var obj = {name: title, activities: 0, hours: 0, Junior: 0, Youth: 0, Adult: 0, veterans: 0,
@@ -91,51 +91,24 @@ function getMonthlyProgramming(relevantDate, done) {
   var buildTables = function() {
 
     // Build the totals table
-    _(counts.totals).forEach(function(totals, club) {
-      /*
-      console.log("This counts: ");
-      console.log(util.inspect(totals));
-      */
-     
-      console.log(club);
-      console.log(totals);
+    TotalsTable.pushObjectRow(counts.totals);
 
-      TotalsTable.pushObjectRow(totals);
-    });
-
-    // Build Nashville table
-    _(counts.nashville).forEach(function(sportsTotals, sport) {
-      /*
+    // Build Total Table
+    _(counts.club).forEach(function(sportsTotals, sport) {
       console.log("This counts: ");
       console.log(util.inspect(sportsTotals));
-      */
+      
 
-      NashvilleTable.pushObjectRow(sportsTotals);
-    });
-
-    // Build At-large table
-    _(counts.statewide).forEach(function(sportsTotals, sport) {
-      /*
-      console.log("This counts: ");
-      console.log(util.inspect(sportsTotals));
-      */
-
-      StatewideTable.pushObjectRow(sportsTotals);
-    });
-
-    // Build Memphis table
-    _(counts.memphis).forEach(function(sportsTotals, sport) {
-      /*
-      console.log("This counts: ");
-      console.log(util.inspect(sportsTotals));
-      */
-
-      MemphisTable.pushObjectRow(sportsTotals);
+      RegionTable.pushObjectRow(sportsTotals);
     });
   };
 
+  var capitalizeString = function(string) {
+    return string[0].toUpperCase() + string.substring(1);
+  }
+
   /* Main program logic */
-  var labelsTotal = ["Clubs", "# of activities", "Hours", "Juniors", "Youth",
+  var labelsTotal = ["# of activities", "Hours", "Juniors", "Youth",
                      "Adult", "Veterans", "Guests", "# of Volunteer Staff",
                       "Volunteer Hours"];
   // The schema
@@ -149,30 +122,25 @@ function getMonthlyProgramming(relevantDate, done) {
                       "Volunteer Hours"];
 
   // Tables
-  var TotalsTable = new xls.Table("Monthly Totals", schema, labelsTotal);
-  var NashvilleTable = new xls.Table("Nashville Breakdown", schema, labelsMonthly);
-  var MemphisTable = new xls.Table("Memphis Breakdown", schema, labelsMonthly);
-  var StatewideTable = new xls.Table("Statewide Breakdown", schema, labelsMonthly);
+  var TotalsTable = new xls.Table("Monthly Totals", schema.slice(1), labelsTotal);
+
+  var regionName = capitalizeString(region);
+  var RegionTable = new xls.Table(regionName + " Breakdown", schema, labelsMonthly);
 
   var counts = {};
   counts.totals = {};
-  counts.nashville = {};
-  counts.memphis = {};
-  counts.statewide = {};
+  counts.club = {};
 
   // Populate totals
-  counts.totals.statewide = genCountsObject("Statewide");
-  counts.totals.nashville = genCountsObject("Nashville");
-  counts.totals.memphis = genCountsObject("Memphis");
+  counts.totals = genCountsObject("Filler");
+  delete counts.totals["name"];
 
   // Generate Counts For Individual Sports
   var sports = ['Bowling', 'Cycling', 'Game Night', 'Goalball', 'Achilles', 'BR dancing', 'Golf', 
                 'Beeper Kickball',
                 'Health Check', 'Exercise and Yoga', 'Golf', 'Conference', 'Other'];
   for (var i = 0; i < sports.length; i++) {
-    counts.nashville[sports[i]] = genCountsObject(sports[i]);
-    counts.memphis[sports[i]] = genCountsObject(sports[i]);
-    counts.statewide[sports[i]] = genCountsObject(sports[i]);
+    counts["club"][sports[i]] = genCountsObject(sports[i]);
   }
 
   //console.log(counts);
@@ -183,68 +151,81 @@ function getMonthlyProgramming(relevantDate, done) {
       Event.loadObjectsByMonth(relevantDate, function(err, objects) {
         if (err) {
           // Inform the callback of the error
-          callback(err);
-        } else {
-          var registrations = [];
-          for (var i = 0; i < objects.length; i++) {
-            //console.log("Start of for loop");
-            //console.log(objects.length);
-            //console.log(i);
-            // Registrations (to send to next function in the list)
+          return callback(err);
+        }
 
-            // Process the event object data
-            var object = objects[i];
+        var registrations = [];
+        for (var i = 0; i < objects.length; i++) {
+          //console.log("Start of for loop");
+          //console.log(objects.length);
+          //console.log(i);
+          // Registrations (to send to next function in the list)
 
-            var sport = getSportNameInContext(object.sport);
-            var club = getClubNameInContext(object.club);
-            var numHours = getDuration(object.start, object.end);
+          // Process the event object data
+          var object = objects[i];
 
-            if (!sport) {
-              // An undefined sport means don't bother with this one, skip it and
-              // move on
+          var sport = getSportNameInContext(object.sport);
+          var club = getClubNameInContext(object.club);
+          
+          // Statewide report includes all clubs. All clubs become statewide;
+          
+          if (region === "statewide") {
+            console.log("Is statewide report nbd, allowing all");
+            var club = "statewide";
+          } else {
+            console.log("Checking if region " + region + " is equal to " + club);
+            if (region !== club) {
+              console.log("Region and club are not equal, continuing");
               continue;
-              // sport = "Run\/Walk";
-            }
-
-            // console.log(club);
-            // console.log(sport);
-
-            /*
-            console.log("Sport: " + sport);
-            console.log("Club: " + club);
-            console.log("Hours: " + numHours);
-            */
-           
-            /* Increment the total number of activities for the specified sport */
-
-            counts.totals[club].activities++;
-            counts.totals[club].hours += numHours;
-            counts[club][sport].activities++;
-            counts[club][sport].hours += numHours;
-
-
-            // Prepare to pass the object to the next function in the waterfall
-            var nextObject = {};
-            nextObject.sport = sport;
-            nextObject.club = club;
-            nextObject.numHours = numHours;
-
-            for (var j = 0; j < object.registrations.length; j++) {
-              //console.log("In registrations " + j);
-              registrations[registrations.length] = {
-                sport: sport,
-                club: club,
-                numHours: numHours,
-                id: object.registrations[j].id
-              }
-
-              //console.log(registrations.length);
             }
           }
 
-          //console.log("Registrations: " + registrations);
-          callback(null, registrations);
+          var numHours = getDuration(object.start, object.end);
+
+          if (!sport) {
+            // An undefined sport means don't bother with this one, skip it and
+            // move on
+            continue;
+          }
+
+          // console.log(club);
+          // console.log(sport);
+
+          /*
+          console.log("Sport: " + sport);
+          console.log("Club: " + club);
+          console.log("Hours: " + numHours);
+          */
+         
+          /* Increment the total number of activities for the specified sport */
+
+          counts.totals.activities++;
+          counts.totals.hours += numHours;
+          counts["club"][sport].activities++;
+          counts["club"][sport].hours += numHours;
+
+
+          // Prepare to pass the object to the next function in the waterfall
+          var nextObject = {};
+          nextObject.sport = sport;
+          nextObject.club = club;
+          nextObject.numHours = numHours;
+
+          for (var j = 0; j < object.registrations.length; j++) {
+            //console.log("In registrations " + j);
+            registrations[registrations.length] = {
+              sport: sport,
+              club: club,
+              numHours: numHours,
+              id: object.registrations[j].id
+            }
+
+            //console.log(registrations.length);
+          }
         }
+
+        //console.log("Registrations: " + registrations);
+        callback(null, registrations);
       });
     },
 
@@ -319,22 +300,22 @@ function getMonthlyProgramming(relevantDate, done) {
           return function(cbinterior) {
             if (!registration.uid) {
               // The registration is anonymous. We will consider this user a guest.
-              counts.totals[registration.club].guests++;
-              counts[registration.club][registration.sport].guests++;
+              counts.totals.guests++;
+              counts["club"][registration.sport].guests++;
               cbinterior(null);
             } else {
               Registration.loadUserObject(registration, function(err, user) {
                 if (err) {
-                  //console.log("ERROR THAT CAUSE CB TWICE: " + err);
-                  cbinterior(err);
+                  // console.log("ERROR THAT CAUSE CB TWICE: " + err);
+                  return cbinterior(err);
                 } else {
                   // Check if the user is veteran
                   //console.log("User checking");
                   //console.log(registration);
 
                   if (user.isVeteran) {
-                    counts.totals[registration.club].veterans++;
-                    counts[registration.club][registration.sport].veterans++;
+                    counts.totals.veterans++;
+                    counts["club"][registration.sport].veterans++;
                   }
 
                   /*
@@ -363,19 +344,19 @@ function getMonthlyProgramming(relevantDate, done) {
                   for (var i = 0; i < roles.length; i++) {
                     if (roles[i] === "volunteer") {
                       // Incrment the number of volunteers, if applicable
-                      counts.totals[registration.club].volunteers++;
-                      counts[registration.club][registration.sport].volunteers++; 
+                      counts.totals.volunteers++;
+                      counts["club"][registration.sport].volunteers++; 
 
                       // This is probably not the right way to calculate volunteer hours
-                      counts.totals[registration.club].volunteerHours += registration.numHours;
-                      counts[registration.club][registration.sport].volunteerHours += registration.numHours;
+                      counts.totals.volunteerHours += registration.numHours;
+                      counts["club"][registration.sport].volunteerHours += registration.numHours;
                       break;
                     }
                   }
 
                   var userAgeGroup = user.ageGroup;
-                  counts.totals[registration.club][userAgeGroup]++;
-                  counts[registration.club][registration.sport][userAgeGroup]++;
+                  counts.totals[userAgeGroup]++;
+                  counts["club"][registration.sport][userAgeGroup]++;
 
                   cbinterior(null);
                 }
@@ -398,35 +379,32 @@ function getMonthlyProgramming(relevantDate, done) {
     if (err) {
       console.log("ERROR");
       console.log(err);
-      done(err);
+      return done(err);
     } else {
       console.log("Building tables");
 
       // Prepare for table completion
       buildTables();
+
       var data = {
         sheet1: {
           name: "TNABA State Monthly Programming Report",
-          data: [TotalsTable, NashvilleTable, StatewideTable, MemphisTable]
+          data: [TotalsTable, RegionTable]
         }
       }
-
-      
-      console.log("Totals Table");
-      console.log(util.inspect(TotalsTable.rows));
-
-      console.log("Nashville Table");
-      console.log(util.inspect(NashvilleTable.rows));
-
-      console.log("At Large Table");
-      console.log(util.inspect(StatewideTable.rows));
-
-      console.log("Memphis Table");
-      console.log(util.inspect(MemphisTable.rows));
       
       done(null, data);
     }
   });
+}
+
+function getMembershipRoster(relevantDate, done) {
+  var memberLabels = ["ID#", "Last Name", "First Name", "Email Address", "Phone Number", "Active",
+                      "Age Group", "Blocked"];
+  var memberSchema = ["number", "string", "string", "string", "string", "boolean", "string", "boolean"];
+
+  var overallLabels = ["New Members", "Total Members"];
+  var overallSchema = ["Number", "Number"];
 }
 
 /*
@@ -732,7 +710,7 @@ function getMonthlyMembership(relevantDate, done) {
       //console.log("Report: " + callbackReport.toString());
       //console.log("Info: " + callbackInfo.toString());
 
-      done(null, data);
+      return done(null, data);
     }
   });
 }
@@ -748,15 +726,15 @@ function getMonthlyMembership(relevantDate, done) {
 module.exports = function(dataType, relevantDate, callback) {
   switch (dataType) {
     case "event": 
-    getMonthlyProgramming(relevantDate, callback);
+    getMonthlyProgramming(relevantDate, "statewide", callback);
     break;
 
     case "eventNashville":
-    getNashvilleProgramming(relevantDate, callback);
+    getMonthlyProgramming(relevantDate, "nashville", callback);
     break;
 
     case "eventMemphis":
-    getMemphisProgramming(relevantDate, callback);
+    getMonthlyProgramming(relevantDate, "memphis", callback);
     break;
 
     case "membership":
@@ -780,13 +758,3 @@ module.exports = function(dataType, relevantDate, callback) {
     break;
   }
 }
-
-
-
-
-
-
-
-
-
-
